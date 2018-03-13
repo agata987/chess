2f0836b234ea468f190e996f734e89d2a0e1a620
agata987 <37309188+agata987@users.noreply.github.com>
2018-03-13 23:47:46 +0100

    Delete krol.cpp

--- krol.cpp
+++
@@ -1,9 +0,0 @@
-#include "krol.h"
-
-Krol::Krol(QWidget *parent, int x, int y, int player, int id) : Figura(parent, x, y, player, id)
-{
-    this->nazwa = "Krol";
-
-    if(player == 1) this->setPixmap(QPixmap(":/biale/PNG/krol_b.png"));
-    else this->setPixmap(QPixmap(":/czarne/PNG/krol_c.png"));
-}
