185f0b42c3bbc0fabfc978e3e9d7880be263dad8
tomkul777 <32366007+tomkul777@users.noreply.github.com>
2018-03-13 23:48:32 +0100

    Delete pionek.cpp

--- pionek.cpp
+++
@@ -1,9 +0,0 @@
-#include "pionek.h"
-
-Pionek::Pionek(QWidget *parent, int x, int y, int player, int id) : Figura(parent, x, y, player, id)
-{
-    this->nazwa = "Goniec";
-
-    if(player == 1) this->setPixmap(QPixmap(":/biale/PNG/pionek_b.png"));
-    else this->setPixmap(QPixmap(":/czarne/PNG/pionek_c.png"));
-}
