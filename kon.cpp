ccf24a2a706fca012ffbb4aff59d6acc629dd56c
agata987 <37309188+agata987@users.noreply.github.com>
2018-03-13 23:47:33 +0100

    Delete kon.cpp

--- kon.cpp
+++
@@ -1,9 +0,0 @@
-#include "kon.h"
-
-Kon::Kon(QWidget *parent, int x, int y, int player, int id) : Figura(parent, x, y, player, id)
-{
-    this->nazwa = "Kon";
-
-    if(player == 1) this->setPixmap(QPixmap(":/biale/PNG/kon_b.png"));
-    else this->setPixmap(QPixmap(":/czarne/PNG/kon_c.png"));
-}
