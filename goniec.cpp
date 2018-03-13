f6864955a05a12b5793b1bb10e547040d653f507
agata987 <37309188+agata987@users.noreply.github.com>
2018-03-13 23:46:32 +0100

    Delete goniec.cpp

--- goniec.cpp
+++
@@ -1,9 +0,0 @@
-#include "goniec.h"
-
-Goniec::Goniec(QWidget *parent, int x, int y, int player, int id) : Figura(parent, x, y, player, id)
-{
-    this->nazwa = "Goniec";
-
-    if(player == 1) this->setPixmap(QPixmap(":/biale/PNG/goniec_b.png"));
-    else this->setPixmap(QPixmap(":/czarne/PNG/goniec_c.png"));
-}
